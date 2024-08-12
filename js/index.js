91e3f965e1809a73abd7b44c7f02686a45d9373c
Kalamyrka <kalamyrka@gmail.com>
2024-08-12 13:07:46 +0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -15,3 +15,33 @@ document.addEventListener('DOMContentLoaded', function () {
       menu.classList.toggle('active');
   });
 });
+
+/* Выпадающий список */
+
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+function myFunctionBook1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+
+function myFunctionBook2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+function myFunctionBook3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+
+// Закройте выпадающее меню, если пользователь щелкает за его пределами
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
